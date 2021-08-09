68af3285e261fcac7c5765f440b0ffbca4ce26fc
Jonathan Cohen <65975460+JDC1492@users.noreply.github.com>
2021-08-09 15:15:02 +0000

    A Simpler way to apply the solution from the last video.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-name = input("What is your name?")
-print(name)
+# name = input("What is your name?")
+# Variable assignment seems to be different than in JS and similar to Ruby
+# print(name)
+
+#A Simpler way to apply the solution from the last video.
+name = input("What is your name? ")
+length = len(name)
+print(length)
